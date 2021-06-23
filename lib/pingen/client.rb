70e6add5a27991e5b0a63c803fed460025b05524
Martin Cavoj <3784696+macav@users.noreply.github.com>
2021-06-23 12:04:24 +0200

    Add delete, pdf and schedule sending

--- lib/pingen/client.rb
+++ lib/pingen/client.rb
@@ -18,7 +18,7 @@ module Pingen
       @logger = logger
     end
 
-    def documents
+    def list
       get_request("/document/list")
     end
 
@@ -28,6 +28,18 @@ module Pingen
 
     def upload(pdf)
       post_multipart_request("/document/upload", pdf, {})
+    end
+
+    def pdf(id)
+      get_request("/document/pdf/id/#{id}")
+    end
+
+    def delete(id)
+      post_request("/document/delete/id/#{id}", {})
+    end
+
+    def schedule_send(id, speed, color: false)
+      post_request("/document/send/id/#{id}", {color: color ? 1 : 0, speed: speed})
     end
 
     def get_request(path, params = nil, request_params = {})
--- spec/pingen/client_spec.rb
+++ spec/pingen/client_spec.rb
@@ -3,8 +3,8 @@ require "dotenv/load"
 RSpec.describe Pingen::Client, :vcr do
   let(:instance) { Pingen::Client.new }
 
-  describe "#documents" do
-    subject(:response) { instance.documents }
+  describe "#list" do
+    subject(:response) { instance.list }
 
     it "can fetch documents list" do
       expect(response).to be_ok
@@ -29,6 +29,44 @@ RSpec.describe Pingen::Client, :vcr do
     it "uploads the file" do
       expect(response).to be_ok
       expect(response.json).to match(a_hash_including(item: a_hash_including(filename: "test.pdf")))
+    end
+  end
+
+  describe "#pdf" do
+    subject(:response) { instance.pdf("335344089") }
+
+    it "returns the PDF" do
+      expect(response).to be_ok
+    end
+  end
+
+  describe "#delete" do
+    subject(:response) { instance.delete("335344089") }
+
+    it "deletes the document" do
+      expect(response).to be_ok
+      expect(response.json).to match(error: false)
+    end
+  end
+
+  describe "#schedule_send" do
+    subject(:response) { instance.schedule_send(id, 2) }
+
+    let(:id) { "804363203" }
+
+    it "sends the document" do
+      expect(response).to be_ok
+      expect(response.json).to match(error: false, id: 84376190)
+    end
+
+    context "when error occurs" do
+      let(:id) { "303870085" }
+
+      it "returns error" do
+        expect(response).not_to be_ok
+        expect(response.code).to eq 400
+        expect(response.json).to match(error: true, errorcode: 2016, errormessage: "No valid country could be detected")
+      end
     end
   end
 end
